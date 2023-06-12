2b8967563975c9741b0c6624b3640df15e05c71a
Mark Baula <57877999+mbaula@users.noreply.github.com>
2023-06-12 17:35:37 -0400

    MaPePeR implementation of brettel function (WIP)

--- src/components/ImageTransformer.js
+++ src/components/ImageTransformer.js
@@ -3,6 +3,124 @@ import React, { useEffect, useState } from 'react';
 const ImageTransformer = ({ selectedImage, selectedImpairment }) => {
   const [transformedImageUrl, setTransformedImageUrl] = useState(null);
 
+  function linearRGB_from_sRGB(v)
+  {
+    var fv = v / 255.0;
+    if (fv < 0.04045) return fv / 12.92;
+    return Math.pow((fv + 0.055) / 1.055, 2.4);
+  }
+
+  function sRGB_from_linearRGB(v)
+  {
+    if (v <= 0.) return 0;
+    if (v >= 1.) return 255;
+    if (v < 0.0031308) return 0.5 + (v * 12.92 * 255);
+    return 0 + 255 * (Math.pow(v, 1.0 / 2.4) * 1.055 - 0.055);
+  }
+
+  var sRGB_to_linearRGB_Lookup = Array(256);
+  (function () {
+      var i;
+      for (i = 0; i < 256; i++) {
+          sRGB_to_linearRGB_Lookup[i] = linearRGB_from_sRGB(i);
+      }
+  })();
+
+   const brettel_params = {
+    protan: {
+        rgbCvdFromRgb_1: [
+            0.14510, 1.20165, -0.34675,
+            0.10447, 0.85316, 0.04237,
+            0.00429, -0.00603, 1.00174
+        ],
+        rgbCvdFromRgb_2: [
+            0.14115, 1.16782, -0.30897,
+            0.10495, 0.85730, 0.03776,
+            0.00431, -0.00586, 1.00155
+        ],
+        separationPlaneNormal: [ 0.00048, 0.00416, -0.00464 ]
+    },
+
+    deutan: {
+        rgbCvdFromRgb_1: [
+            0.36198, 0.86755, -0.22953,
+            0.26099, 0.64512, 0.09389,
+           -0.01975, 0.02686, 0.99289,
+        ],
+        rgbCvdFromRgb_2: [
+            0.37009, 0.88540, -0.25549,
+            0.25767, 0.63782, 0.10451,
+           -0.01950, 0.02741, 0.99209,
+        ],
+        separationPlaneNormal: [ -0.00293, -0.00645, 0.00938 ]
+    },
+
+    tritan: {
+        rgbCvdFromRgb_1: [
+            1.01354, 0.14268, -0.15622,
+           -0.01181, 0.87561, 0.13619,
+            0.07707, 0.81208, 0.11085,
+        ],
+        rgbCvdFromRgb_2: [
+            0.93337, 0.19999, -0.13336,
+            0.05809, 0.82565, 0.11626,
+            -0.37923, 1.13825, 0.24098,
+        ],
+        separationPlaneNormal: [ 0.03960, -0.02831, -0.01129 ]
+    },
+  };
+
+  function brettel(srgb, t, severity) {
+    
+    // Go from sRGB to linearRGB
+    var rgb = Array(3);
+    rgb[0] = sRGB_to_linearRGB_Lookup[srgb[0]];
+    rgb[1] = sRGB_to_linearRGB_Lookup[srgb[1]];
+    rgb[2] = sRGB_to_linearRGB_Lookup[srgb[2]];
+    console.log('RGB', srgb);
+  
+    var params = brettel_params[t];
+    var separationPlaneNormal = params['separationPlaneNormal'];
+    var rgbCvdFromRgb_1 = params['rgbCvdFromRgb_1'];
+    var rgbCvdFromRgb_2 = params['rgbCvdFromRgb_2'];
+  
+    // Check on which plane we should project by comparing with the separation plane normal.
+    var dotWithSepPlane =
+      rgb[0] * separationPlaneNormal[0] +
+      rgb[1] * separationPlaneNormal[1] +
+      rgb[2] * separationPlaneNormal[2];
+    var rgbCvdFromRgb = dotWithSepPlane >= 0 ? rgbCvdFromRgb_1 : rgbCvdFromRgb_2;
+  
+    // Transform to the full dichromat projection plane.
+    var rgb_cvd = Array(3);
+    rgb_cvd[0] =
+      rgbCvdFromRgb[0] * rgb[0] +
+      rgbCvdFromRgb[1] * rgb[1] +
+      rgbCvdFromRgb[2] * rgb[2];
+    rgb_cvd[1] =
+      rgbCvdFromRgb[3] * rgb[0] +
+      rgbCvdFromRgb[4] * rgb[1] +
+      rgbCvdFromRgb[5] * rgb[2];
+    rgb_cvd[2] =
+      rgbCvdFromRgb[6] * rgb[0] +
+      rgbCvdFromRgb[7] * rgb[1] +
+      rgbCvdFromRgb[8] * rgb[2];
+  
+    // Apply the severity factor as a linear interpolation.
+    // It's the same to do it in the RGB space or in the LMS
+    // space since it's a linear transform.
+    rgb_cvd[0] = rgb_cvd[0] * severity + rgb[0] * (1.0 - severity);
+    rgb_cvd[1] = rgb_cvd[1] * severity + rgb[1] * (1.0 - severity);
+    rgb_cvd[2] = rgb_cvd[2] * severity + rgb[2] * (1.0 - severity);
+  
+    // Go back to sRGB
+    return [
+      sRGB_from_linearRGB(rgb_cvd[0]),
+      sRGB_from_linearRGB(rgb_cvd[1]),
+      sRGB_from_linearRGB(rgb_cvd[2]),
+    ];
+  }
+
   const transformImage = async (image, impairment) => {
     switch (impairment) {
       case 'protanopia':
@@ -36,9 +154,52 @@ const ImageTransformer = ({ selectedImage, selectedImpairment }) => {
   }, [selectedImage, selectedImpairment]);
 
   const applyProtanopiaTransformation = (image) => {
-    // Transformation logic for protanopia
-    // Replace with your implementation
-    return URL.createObjectURL(image);
+    return new Promise((resolve, reject) => {
+      const canvas = document.createElement('canvas');
+      const ctx = canvas.getContext('2d');
+      const img = new Image();
+      img.src = URL.createObjectURL(image);
+  
+      img.onload = () => {
+        canvas.width = img.width;
+        canvas.height = img.height;
+  
+        ctx.drawImage(img, 0, 0);
+  
+        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        const data = imageData.data;
+        for (let i = 0; i < data.length; i += 4) {
+          const red = data[i];
+          const green = data[i + 1];
+          const blue = data[i + 2];
+
+          // Convert sRGB to linear RGB
+          const linearRed = linearRGB_from_sRGB(red);
+          const linearGreen = linearRGB_from_sRGB(green);
+          const linearBlue = linearRGB_from_sRGB(blue);
+
+          // Apply the Brettel transformation for protanopia
+          const transformedRGB = brettel([linearRed, linearGreen, linearBlue], 'protan', 1.0);
+
+          // Convert linear RGB back to sRGB
+          const transformedRed = sRGB_from_linearRGB(transformedRGB[0]);
+          const transformedGreen = sRGB_from_linearRGB(transformedRGB[1]);
+          const transformedBlue = sRGB_from_linearRGB(transformedRGB[2]);
+  
+          const avg = 0.299 * red + 0.587 * green + 0.114 * blue;
+          data[i] = avg; // red
+          data[i + 1] = avg; // green
+          data[i + 2] = avg; // blue
+        }
+        ctx.putImageData(imageData, 0, 0);
+  
+        resolve(canvas.toDataURL());
+      };
+  
+      img.onerror = () => {
+        reject(new Error('Failed to load image.'));
+      };
+    });
   };
 
   const applyDeuteranopiaTransformation = (image) => {
